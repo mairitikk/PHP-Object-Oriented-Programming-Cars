d00f8f413903ff86c5a2b08403f8bda99bebd76d
mairi tikk <mairi.tikk@gmail.com>
2024-06-25 09:21:33 +0200

    change objects

--- objects.php
+++ objects.php
@@ -1,11 +1,18 @@
- <?php $vehicles =
-        [
-            ["Astro", "Estrella", "2021", 500, 50000, "veh-01.jpg"],
-            ["Terraza", "Spinneo", "2020", 30000, 31000, "veh-02.jpg"],
-            ["Sage", "Ecostar", "2014", 70000, 15000, "veh-03.jpg"]
-        ];
+ <?php
+
+
     class Vehicle
     {
+
+        function __construct($make, $model, $year, $mileage, $price, $image)
+        {
+            $this->make = $make;
+            $this->model = $model;
+            $this->year = $year;
+            $this->mileage = $mileage;
+            $this->price = $price;
+            $this->image = $image;
+        }
         var $make;
         var $model;
         var $year;
@@ -13,20 +20,11 @@
         var $price;
         var $image;
     };
-    $vehicle1 = new Vehicle;
-    $vehicle1->make = "Astro";
-    $vehicle1->model = "Estrella";
-    $vehicle1->year = "2021";
-    $vehicle1->mileage = 500;
-    $vehicle1->price = 50000;
-    $vehicle1->image = "veh-01.jpg";
+    $vehicle1 = new Vehicle("Astro", "Estrella", "2021", 500, 50000, "veh-01.jpg");
 
-    $vehicle2 = new Vehicle;
-    $vehicle2->make = "Terraza";
-    $vehicle2->model = "Spinneo";
-    $vehicle2->year = "2020";
-    $vehicle2->mileage = 30000;
-    $vehicle2->price = 31000;
-    $vehicle2->image = "veh-02.jpg";
 
+    $vehicle2 = new Vehicle("Terraza", "Spinneo", "2020", 30000, 31000, "veh-02.jpg");
+    $vehicle3 = new Vehicle("Sage", "Ecostar", "2014", 70000, 15000, "veh-03.jpg");
+
+    $veicles = [$vehicle1, $vehicle2, $vehicle3];
     ?>
